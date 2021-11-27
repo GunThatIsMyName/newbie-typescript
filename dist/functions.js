f15e8ac6b3a95990c3a61e3b188558c9fd1f44d8
Junhyun <mnmthatisus@gmail.com>
2021-11-27 12:07:26 +0900

    shopping:

---
+++ dist/functions.js
@@ -0,0 +1,5 @@
+"use strict";
+const ho = (a, b) => {
+    return a + (b || 0);
+};
+console.log(ho(2));
--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const a:IA ={
 
 a.x ="minji";
 a.t =2;
-console.log(a,"@@@");
+// console.log(a,"@@@");
 
 // 
 
@@ -36,3 +36,56 @@ const sum:Sumb =(a,b)=>{
 
 const hello = sum.prop1="miniㅇㅇㅇji";
 
+const ho = (a: number, b?: number): number => {
+    return a + (b||0);
+  };
+  
+  console.log(ho(2,2));
+
+
+  const every =(numbers:number[]):number=>{
+    return numbers.reduce((result,num)=>result+num,0)
+  }
+const arr12=[1,2,3,4,5];
+  console.log(every(arr12))
+
+
+//   
+// 
+// 
+
+class Robot {
+    name:string;
+    subname:string;
+    constructor(name:string,subname:string){
+        this.name = name;
+        this.subname=subname;
+    }
+
+    aksName(){
+        console.log(`my name is ${this.name}  || my subname is ${this.subname}  !`);
+    }
+
+    move(distance:number){
+        console.log(`${this.name} moved ${distance} meters`);
+    }
+}
+
+class FlyingRobot extends Robot{
+    jetpackSize:number;
+    constructor(name:string,jetpackSize:number,subname:string){
+        super(name,subname);
+        this.jetpackSize=jetpackSize;
+    }
+    move(distance:number){
+        console.log(`${this.name} is flying`);
+        super.move(distance);
+    }
+}
+
+const robot = new Robot("Minji","mandy");
+robot.aksName();
+
+const fly = new FlyingRobot("cooper",2,"민지");
+console.log(fly);
+fly.move(3);
